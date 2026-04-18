45b79686d64dbd16755f0a524a30961b0c67af64
VincentNdegwa <ndegwavincent7@gmail.com>
2026-04-18 11:21:20 +0300

    feat: brand and name setup

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -40,6 +40,7 @@ class HandleInertiaRequests extends Middleware
         return [
             ...parent::share($request),
             'name' => config('app.name'),
+            'brand' => config('app.brand'),
             'auth' => [
                 'user' => $user,
             ],
--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import AppLogoIcon from '@/components/AppLogoIcon.vue';
+import { usePage } from '@inertiajs/vue3';
+
+const page = usePage();
 </script>
 
 <template>
@@ -10,7 +13,8 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
         <span class="mb-0.5 truncate leading-tight font-semibold"
-            >Laravel Starter Kit</span
+            >{{ page.props.name }}</span
         >
+        <small style="font-size: 8px;" >Powered By {{ page.props.brand }}</small>
     </div>
 </template>
